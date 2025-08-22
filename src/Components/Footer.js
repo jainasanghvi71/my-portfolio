084b71d3480d9569956021919e546d99f8d21a5e
jainasanghvi71 <jainamehta71@gmail.com>
2025-08-22 12:54:41 +0530

    A portfolio added

--- src/Components/Footer.js
+++ src/Components/Footer.js
@@ -10,11 +10,11 @@ function Footer() {
           target="_blank"
           rel="noreferrer noopener"
         >
-          Pavan MG
+          Jaina Sanghvi
         </a>{" "}
         with
         <span className="text-gradient font-medium"> Love</span> &
-        <span className="text-gradient font-medium"> Coffee</span>
+        <span className="text-gradient font-medium"> Tea</span>
       </p>
     </footer>
   );
--- src/Details.js
+++ src/Details.js
@@ -1,9 +1,9 @@
 // Enter all your detials in this file
 // Logo images
-import logogradient from "./assets/logo.svg";
-import logo from "./assets/logo2.svg";
+import logogradient from "./assets/js-logo.svg";
+import logo from "./assets/js-logo2.svg";
 // Profile Image
-import profile from "./assets/profile.jpg";
+import profile from "./assets/photo.png";
 // Tech stack images
 import html from "./assets/techstack/html.png";
 import css from "./assets/techstack/css.png";
@@ -35,15 +35,21 @@ export const logos = {
 
 // Enter your Personal Details here
 export const personalDetails = {
-  name: "Pavan MG",
-  tagline: "I build things for web",
+  name: "Jaina Sanghvi",
+  tagline: "I build things for web.",
   img: profile,
-  about: `Lorem Ipsum is simply dummy text of the printing and typesetting industry. Lorem Ipsum has been the industry's standard dummy text ever since the 1500s, when an unknown printer took a galley of type and scrambled it to make a type specimen book.`,
+  about: `A self-motivated, results-driven, and customer-focused Senior Software Engineer with over 10 years of experience in the 
+IT industry. Adept at full-stack development with deep expertise in system design, integration, and solving complex 
+business problems. Skilled in converting requirements into scalable technical solutions, with strong hands-on experience 
+in database architecture, enterprise systems, and cloud-based platforms. Known for clear communication, collaborative 
+leadership, and delivering high-impact solutions. Passionately interested in research and development, with a growing 
+focus on artificial intelligence and emerging technologies. Keen to contribute in roles that blend innovation, technical 
+depth, and real-world impact`,
 };
 
 // Enter your Social Media URLs here
 export const socialMediaUrl = {
-  linkdein: "https://www.linkedin.com/",
+  linkdein: "https://www.linkedin.com/in/jaina-sanghvi/",
   github: "https://www.github.com/",
   twitter: "https://twitter.com/",
   instagram: "https://www.instagram.com/",
@@ -52,43 +58,36 @@ export const socialMediaUrl = {
 // Enter your Work Experience here
 export const workDetails = [
   {
-    Position: "Frontend Web Developer",
-    Company: `Company Name here`,
-    Location: "Bengaluru",
+    Position: "React Developer",
+    Company: `TCS`,
+    Location: "Gandhinagar",
     Type: "Full Time",
-    Duration: "Sep 2021 - Dec 2021",
+    Duration: "May 2022 - Present",
   },
   {
-    Position: "Internship",
-    Company: `Company Name here`,
-    Location: "Bengaluru",
-    Type: "Internship",
-    Duration: "Sep 2021 - Dec 2021",
+    Position: "Senior Software Engineer",
+    Company: `Xtreme Technology`,
+    Location: "Rajkot",
+    Type: "Full Time",
+    Duration: "May 2016 - Feb 2022",
   },
   {
-    Position: "Internship",
-    Company: `Company Name here`,
-    Location: "Bengaluru",
-    Type: "Internship",
-    Duration: "Sep 2021 - Dec 2021",
+    Position: "Software Engineer",
+    Company: `NxSol Private Limited`,
+    Location: "Rajkot",
+    Type: "Full Time",
+    Duration: "Nov 2013 - Apr 2015",
   },
 ];
 
 // Enter your Education Details here
 export const eduDetails = [
   {
-    Position: "Frontend Development",
-    Company: "Udemy, YouTube, Google, Medium",
-    Location: "Online",
+    Position: "Bachelor in Computer Engineering",
+    Company: `Darshan University`,
+    Location: "Rajkot",
     Type: "Full Time",
-    Duration: "Jan 2022 - Present",
-  },
-  {
-    Position: "Bachelor in Electronics & Communication",
-    Company: `Your College Name here`,
-    Location: "Bengaluru",
-    Type: "Full Time",
-    Duration: "Aug 2020 - Present",
+    Duration: "May 2010 - Apr 2013",
   },
 ];
 
